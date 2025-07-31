3499592167793d3026f6c92978ec47029e9267ae
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-07-31 01:04:21 -0700

    Implemented simple cut and paste

--- game/gmtkgamejam2025/apps/photoshop_app/photoshop_app.gd
+++ game/gmtkgamejam2025/apps/photoshop_app/photoshop_app.gd
@@ -19,11 +19,8 @@ extends Control
 @onready var pasted_selection: Polygon2D = %PastedSelection
 @onready var cutter: Polygon2D = %Cutter
 
-@onready var testbutton: Button = $PanelContainer/VBoxContainer/Body/EditingPanel/EditingAnchor/TESTBUTTON
-@onready var testbutton_2: Button = $PanelContainer/VBoxContainer/Body/EditingPanel/EditingAnchor/TESTBUTTON2
-
+## Panel movement/zoom
 var previous_mouse_position: Vector2
-
 const ZOOM_LEVELS: Array = [
 	0.5,
 	1,
@@ -31,17 +28,20 @@ const ZOOM_LEVELS: Array = [
 	3,
 	4,
 ]
-
 var current_zoom_index: int = 1
+
+## Copied texture
+var copied_vertices: PackedVector2Array
+var copied_texture: Texture2D
+var copied_offset: Vector2
+
+## Moving pasted selection
+var is_pasted_moveable: bool = false
 
 func _ready() -> void:
 	await get_tree().process_frame
 	editable_image.texture_offset = editable_image.texture.get_size() / 2
-	editable_image.position = -editable_image.texture.get_size() / 2
 	editing_anchor.position = editing_panel.size / 2
-	
-	testbutton_2.pressed.connect(_show_cutter)
-	testbutton.pressed.connect(_on_test_button_pressed)
 
 func _process(delta: float) -> void:
 	handle_actions(delta)
@@ -50,13 +50,14 @@ func _process(delta: float) -> void:
 
 func handle_actions(delta: float) -> void:
 	if Input.is_action_just_pressed("copy"):
-		pass
+		copy_selection()
 	
 	if Input.is_action_just_pressed("cut"):
-		pass
+		copy_selection()
+		delete_selection()
 	
 	if Input.is_action_just_pressed("paste"):
-		pass
+		paste_selection()
 
 func handle_movement(delta: float) -> void:
 	var current_mouse_position: Vector2 = editing_panel.get_local_mouse_position()
@@ -75,6 +76,23 @@ func handle_movement(delta: float) -> void:
 		elif editing_anchor.position.y > editing_panel.size.y:
 			editing_anchor.position.y = editing_panel.size.y
 	
+	if Input.is_action_just_pressed("drag_pasted_selection"):
+		var local_mouse_pos: Vector2 = current_mouse_position - editing_anchor.position
+		var translated_selection_polygon: PackedVector2Array = pasted_selection.polygon
+		for i in translated_selection_polygon.size():
+			translated_selection_polygon[i] += pasted_selection.position
+		if Geometry2D.is_point_in_polygon(local_mouse_pos, translated_selection_polygon):
+			is_pasted_moveable = true
+			print("Yes")
+		else:
+			is_pasted_moveable = false
+			## TODO: MERGE THE SELECTION WITH THE BASE IMAGE
+	
+	if Input.is_action_pressed("drag_pasted_selection") and is_pasted_moveable and not is_zero_approx(diff.length()):
+		#for i in pasted_selection.polygon.size():
+			#pasted_selection.polygon[i] += diff
+		pasted_selection.position += diff
+	
 	previous_mouse_position = current_mouse_position
 
 func handle_zoom(delta: float) -> void:
@@ -90,8 +108,18 @@ func handle_zoom(delta: float) -> void:
 			current_zoom_index = 0
 		editing_anchor.scale = Vector2(ZOOM_LEVELS[current_zoom_index], ZOOM_LEVELS[current_zoom_index])
 
-func _on_test_button_pressed() -> void:
+func copy_selection() -> PackedVector2Array:
+	var new_polygon = Geometry2D.intersect_polygons(editable_image.polygon, cutter.polygon)
+	if new_polygon.is_empty():
+		return []
 	
+	copied_offset = editable_image.texture_offset
+	copied_vertices = new_polygon[0]
+	copied_texture = editable_image.texture
+	
+	return new_polygon[0]
+
+func delete_selection() -> void:
 	var new_polygon = Geometry2D.intersect_polygons(editable_image.polygon, cutter.polygon)
 	if new_polygon.is_empty():
 		return
@@ -99,7 +127,10 @@ func _on_test_button_pressed() -> void:
 	var newnew_polygon = Geometry2D.clip_polygons(editable_image.polygon, new_polygon[0])
 	if newnew_polygon.is_empty():
 		return
+	
 	editable_image.polygon = newnew_polygon[0]
 
-func _show_cutter() -> void:
-	cutter.visible = !cutter.visible
+func paste_selection() -> void:
+	pasted_selection.texture_offset = copied_offset
+	pasted_selection.polygon = copied_vertices
+	pasted_selection.texture = copied_texture
