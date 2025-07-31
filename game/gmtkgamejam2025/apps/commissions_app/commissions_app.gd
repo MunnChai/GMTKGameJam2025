11a52eea4aa56eaf545fb81ef178f41ec3b0dac3
Eva <evalian050917@gmail.com>
2025-07-31 01:09:11 -0700

    ui updates

--- game/gmtkgamejam2025/apps/commissions_app/commissions_app.gd
+++ game/gmtkgamejam2025/apps/commissions_app/commissions_app.gd
@@ -1,18 +1,25 @@
 class_name CommissionApp
 extends DesktopWindow
 
+# TODO for commission app
+# update asset list (working with file explorer maybe)
+# submit work
+# generate rating
+
 @onready var id: RichTextLabel = %Id
 @onready var title: RichTextLabel = %Title
 @onready var desc: RichTextLabel = %Description
 
 @onready var download_button: Button = %Download
 @onready var submit_button: Button = %Submit
+@onready var asset_list: HBoxContainer = %AssetList
+
+@export var commissions: Dictionary[int, CommissionStat]
 
 var day: int
 var commission_stat: CommissionStat
 
-@export var commissions: Dictionary[int, CommissionStat]
-
+signal day_changed
 
 func _ready() -> void:
 	if not day:
@@ -30,6 +37,7 @@ func on_download_pressed() -> void:
 func on_submit_pressed() -> void:
 	day = day + 1
 	update_comm()
+	day_changed.emit()
 
 func update_comm() -> void:
 	var stat: CommissionStat = commissions[day]
@@ -37,6 +45,10 @@ func update_comm() -> void:
 		return
 	commission_stat = stat
 	
-	id.text = "User " + stat.id + "\n "
-	title.text = stat.title + "\n "
-	desc.text = stat.desc + "\n "
+	id.text = "User [b]" + stat.id + "[/b]"
+	title.text = "[b][i]" + stat.title + "[/i][/b]"
+	desc.text = stat.desc
+	
+	var assets: Array[Texture2D] = stat.assets
+	if not assets:
+		return
