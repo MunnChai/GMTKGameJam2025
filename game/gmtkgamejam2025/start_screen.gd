d714093a187e6a93bb00743164541a0aeaf550d7
Eva <evalian050917@gmail.com>
2025-08-01 22:27:48 -0700

    start screen

---
+++ game/gmtkgamejam2025/start_screen.gd
@@ -0,0 +1,12 @@
+class_name StartScreen
+extends Node2D
+
+@onready var start_button: Button = %StartButton
+
+const DesktopScene = preload("res://desktop/desktop.tscn")
+
+func _ready() -> void:
+	start_button.pressed.connect(on_start_pressed)
+	
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(DesktopScene)
