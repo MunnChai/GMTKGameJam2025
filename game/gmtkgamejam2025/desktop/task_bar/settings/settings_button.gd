bef91239f2be470172dca5c4c6e3e23c3ac2878f
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-08-01 17:45:44 -0700

    Some polish

--- game/gmtkgamejam2025/desktop/task_bar/settings/settings_button.gd
+++ game/gmtkgamejam2025/desktop/task_bar/settings/settings_button.gd
@@ -2,9 +2,15 @@ class_name SettingsButton
 extends TextureButton
 
 @onready var settings_panel: SettingsPanel = %SettingsPanel
+@onready var settings_button_text: RichTextLabel = %SettingsButtonText
 
 func _ready() -> void:
 	pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
 	settings_panel.visible = !settings_panel.visible
+	
+	if settings_panel.visible:
+		settings_button_text.add_theme_color_override("default_color", Color.WHITE)
+	else:
+		settings_button_text.add_theme_color_override("default_color", Color.BLACK)
