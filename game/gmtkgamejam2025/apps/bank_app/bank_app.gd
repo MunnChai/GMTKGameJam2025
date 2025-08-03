bb583756f5f2aff4a2dc4ecaeab83ed576273079
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-08-02 18:07:48 -0600

    Added due date update

--- game/gmtkgamejam2025/apps/bank_app/bank_app.gd
+++ game/gmtkgamejam2025/apps/bank_app/bank_app.gd
@@ -2,9 +2,15 @@ class_name BankApp
 extends Control
 
 @onready var balance_label: RichTextLabel = %Balance
+@onready var due_date_label: RichTextLabel = %DueDate
 
 func _ready() -> void:
 	GameStateManager.money_changed.connect(set_balance)
+	GameStateManager.day_changed.connect(set_day)
+	set_day(GameStateManager.day)
 
 func set_balance(balance: float) -> void:
 	balance_label.text = "$" + str("%0.2f" % balance)
+
+func set_day(day: int) -> void:
+	due_date_label.text = "Due in " + str(5 - day) + " days"
