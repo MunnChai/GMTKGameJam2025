489e1a037aef691da77d60e2e8a0923a94c92e7d
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2025-08-02 18:13:46 -0600

    Small change to fix small bug

--- game/gmtkgamejam2025/start_screen/start_screen.gd
+++ game/gmtkgamejam2025/start_screen/start_screen.gd
@@ -8,12 +8,12 @@ const DesktopScene = preload("res://desktop/desktop.tscn")
 
 func _ready() -> void:
 	player_input.text_submitted.connect(on_input_submitted)
+	player_input.text_changed.connect(_on_text_changed)
 	start_button.pressed.connect(on_start_pressed)
 
-func _process(delta: float) -> void:
-	if player_input.text != "":
-		start_button.disabled = false
-	
+func _on_text_changed(new_text: String) -> void:
+	start_button.disabled = new_text == ""
+
 func on_input_submitted(text: String) -> void:
 	on_start_pressed()
 	
