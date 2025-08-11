2d8d15b7c220fbbf6b685e6e9092d04d77b13ad6
felix liu <felix.liu.ray@gmail.com>
2025-08-11 13:09:39 -0700

    Make settings auto-close between days, tweak task bar arrangement to not pop every shortcut on changes

--- game/gmtkgamejam2025/desktop/task_bar/settings/settings_button.gd
+++ game/gmtkgamejam2025/desktop/task_bar/settings/settings_button.gd
@@ -11,17 +11,26 @@ func _ready() -> void:
 	pivot_offset = size / 2.0
 	
 	pressed.connect(_on_pressed)
+	
+	GameStateManager.day_changed.connect(_on_day_changed)
+
+func _on_day_changed(new_day: int) -> void:
+	## Close the settings menu for the next day
+	if is_open:
+		toggle_open()
+		button_pressed = false
 
 func _on_pressed() -> void:
-	is_open = !is_open
-	
 	if not GameSettings.get_setting("reduced_motion", false, "graphics"):
 		TweenUtil.pop_delta(self, Vector2(0.2, 0.2), 0.3)
 	
 	SoundManager.play_global_oneshot(&"ui_basic_click")
 
-	
+	toggle_open()
 	#settings_panel.visible = !settings_panel.visible
+
+func toggle_open() -> void:
+	is_open = !is_open
 	
 	if is_open:
 		#settings_button_text.add_theme_color_override("default_color", Color.WHITE)
